865ca5e07bb7f4f55480dbc75ff6dcd80f381aad
Sophia Zhi <sophiazhi@gmail.com>
2019-08-05 15:40:06 -0400

    some game setup

--- gerrymandering_game/src/App.js
+++ gerrymandering_game/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numDistricts: props.numDistricts,
+            numPopulations: props.numPopulations,
+            district: 0, // increases by 1 every click
+        };
+    }
 }
 
-export default App;
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numDistricts: props.numDistricts,
+            numPopulations: props.numPopulations,
+        };
+    }
+
+    render() {
+        return null;
+    }
+}
+
+export default Game;
