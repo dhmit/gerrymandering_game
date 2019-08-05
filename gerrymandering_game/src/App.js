da89a80e3594760a885743642e5a97346f524dfb
Sophia Zhi <sophiazhi@gmail.com>
2019-08-05 16:55:02 -0400

    Update App.js

--- gerrymandering_game/src/App.js
+++ gerrymandering_game/src/App.js
@@ -4,12 +4,30 @@ import './App.css';
 class Square extends React.Component {
     constructor(props) {
         super(props);
+        const populations = {};
+        let total = 0;
+        for (let i = 0; i < props.numPopulations; i++) {
+            populations[i] = Math.random();
+            total += populations[i];
+        }
+        for (let i = 0; i < props.numPopulations; i++) {
+            populations[i] = 100 * populations[i] / total;
+        }
+
         this.state = {
             numDistricts: props.numDistricts,
             numPopulations: props.numPopulations,
             district: 0, // increases by 1 every click
+            populations: populations,
         };
     }
+
+    render() {
+        // display a square with population distributions written over as text
+        // on click, changes color to indicate changed district
+        return null;
+    }
+
 }
 
 class Game extends React.Component {
