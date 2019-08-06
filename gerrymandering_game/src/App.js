7284cc07af705966670454bbc6727eec6d368274
Sophia Zhi <sophiazhi@gmail.com>
2019-08-06 12:55:02 -0400

    split squares into columns?

--- gerrymandering_game/src/App.js
+++ gerrymandering_game/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Col from 'react-bootstrap/Col';
 
 class Square extends React.Component {
     constructor(props) {
@@ -61,10 +62,28 @@ class Map extends React.Component {
         const viewSquares = this.state.squares.map(square =>
             <Square district={square.district} populations={square.populations}/>
         );
+
+        let column1, column2, column3;
+        const third = Math.round(viewSquares.length / 3);
+        if (viewSquares.length % 3 === 0) {
+            column1 = viewSquares.slice(0, third);
+            column2 = viewSquares.slice(third, 2*third);
+            column3 = viewSquares.slice(2*third, 3*third);
+        } else if (viewSquares.length % 3 === 1) {
+            column1 = viewSquares.slice(0, third+1);
+            column2 = viewSquares.slice(third+1, 2*third+1);
+            column3 = viewSquares.slice(2*third+1, 3*third+1);
+        } else {
+            column1 = viewSquares.slice(0, third+1);
+            column2 = viewSquares.slice(third+1, 2*third+2);
+            column3 = viewSquares.slice(2*third+2, 3*third+2);
+        }
         
         return (
             <div>
-                {viewSquares}
+                <Col>{column1}</Col>
+                <Col>{column2}</Col>
+                <Col>{column3}</Col>
             </div>
         );
     }
